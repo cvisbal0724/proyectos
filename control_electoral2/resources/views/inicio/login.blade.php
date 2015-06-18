0e3ec7932e192ad3cd04705731fd36de6b886be2
cvisbal0724 <cvisbal0724@gmail.com>
2015-06-18 16:23:04 -0500

    Cambio diseño control

--- control_electoral2/resources/views/inicio/login.blade.php
+++ control_electoral2/resources/views/inicio/login.blade.php
@@ -1,35 +1,73 @@
- <div id="login-page">
-       
-<div class="container">
-        <div class="row">
-            <div class="col-md-4 col-md-offset-4">
-                <div class="login-panel panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title">Please Sign In</h3>
-                    </div>
-                    <div class="panel-body">
-                        
-                            <fieldset>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Usuario" type="text" ng-model='loginVO.usuario' autofocus>
-                                </div>
-                                <div class="form-group">
-                                    <input class="form-control" placeholder="Clave" name="password" type="password" ng-model='loginVO.clave'
+
+   <div id="login-page">
+     
+    <div class="container">
+       <form class="form-login">
+                <h2 class="form-login-heading">Entrar ahora</h2>
+                <div class="login-wrap">
+                    <input class="form-control" placeholder="Usuario" type="text" ng-model='loginVO.usuario' autofocus>
+                    <br>
+                   <input class="form-control" placeholder="Contraseña" name="password" type="password" ng-model='loginVO.clave'
                                     ng-keyup="$event.keyCode == 13 && loguear()">
-                                </div>
-                                <div class="checkbox">
-                                    <label>
-                                        <input name="remember" type="checkbox" value="Remember Me">Remember Me
-                                    </label>
-                                </div>
-                                <!-- Change this to a button or input when using this as a form -->
-                                <input id='hd_token' type='hidden' value='[[csrf_token()]]'>
-                                <a href="" class="btn btn-lg btn-success btn-block" ng-click='loguear()'>Entrar</a>
-                            </fieldset>
-                      
+
+                    <label class="checkbox">
+                        <span class="pull-right">
+                            <a data-toggle="modal" href="login.html#myModal"> Recordar Contraseña?</a>
+            
+                        </span>
+                    </label>
+                     <input id='hd_token' type='hidden' value='[[csrf_token()]]'>
+                    <a class="btn btn-theme btn-block" href="" ng-click='loguear()'><i class="fa fa-lock"></i> ENTRAR</a>
+                    
+                    <!--<hr>
+                    
+                    <div class="login-social-link centered">
+                    <p>or you can sign in via your social network</p>
+                        <button class="btn btn-facebook" type="submit"><i class="fa fa-facebook"></i> Facebook</button>
+                        <button class="btn btn-twitter" type="submit"><i class="fa fa-twitter"></i> Twitter</button>
                     </div>
+                    <div class="registration">
+                        Don't have an account yet?<br/>
+                        <a class="" href="#">
+                            Create an account
+                        </a>
+                    </div>-->
+        
                 </div>
-            </div>
+        
+                  <!-- Modal -->
+                  <div aria-hidden="true" aria-labelledby="myModalLabel" role="dialog" tabindex="-1" id="myModal" class="modal fade">
+                      <div class="modal-dialog">
+                          <div class="modal-content">
+                              <div class="modal-header">
+                                  <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                  <h4 class="modal-title">Recordar Contraseña?</h4>
+                              </div>
+                              <div class="modal-body">
+                                  <p>Enter your e-mail address below to reset your password.</p>
+                                  <input type="text" name="email" placeholder="Email" autocomplete="off" class="form-control placeholder-no-fix">
+        
+                              </div>
+                              <div class="modal-footer">
+                                  <button data-dismiss="modal" class="btn btn-default" type="button">Cancel</button>
+                                  <button class="btn btn-theme" type="button">Submit</button>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <!-- modal -->
+        
+              </form>       
         </div>
-    </div>
-    </div>
+        </div>
+
+   <!-- js placed at the end of the document so the pages load faster -->
+    <script src="app_cliente/js/jquery.js"></script>
+    <script src="app_cliente/js/bootstrap.min.js"></script>
+
+    <!--BACKSTRETCH-->
+    <!-- You can use an image of whatever size. This script will stretch to fit in any screen size.-->
+    <script type="text/javascript" src="app_cliente/js/jquery.backstretch.min.js"></script>
+    <script>
+        $.backstretch("app_cliente/img/login-bg.jpg", {speed: 500});
+    </script> 
